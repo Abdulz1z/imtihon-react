e7282b7fd0753a89d6d5fb92cde1d312c6028036
AbdulazizObidov <abdulazizobidov03@gmail.com>
2023-04-14 16:30:04 +0500

    header

--- src/App.js
+++ src/App.js
@@ -1,45 +1,11 @@
 /** @format */
 
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 
-const data = [
-   {
-      id: 1,
-      title: "javascript",
-      body: "javascript base",
-      id: 2,
-      title: "javascript",
-      body: "javascript base",
-      id: 3,
-      title: "javascript",
-      body: "javascript base",
-      id: 4,
-      title: "javascript",
-      body: "javascript base",
-      id: 5,
-      title: "javascript",
-      body: "javascript base",
-   },
-];
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
    return (
-      <div className="App">
-         <form className="form d-flex align-items-center justify-content-center mt-4 ">
-            <input
-               type="text"
-               placeholder="title"
-               className="w-25"
-            />
-            <input
-               type="text"
-               placeholder="body"
-               className="w-25"
-            />
-            <button className="border-success border-2">Add</button>
-         </form>
-      </div>
+     <div></div>
    );
 }
 
